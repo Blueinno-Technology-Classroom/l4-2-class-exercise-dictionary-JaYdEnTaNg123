1c5dc0d00d5576d2d7dc82987281603405e06884
JdEnTaNg999 <tangjayden999@gmail.com>
2024-03-02 12:21:18 +0800

    First Lesson :P

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
-roblox {}
+bloxfuits = {
+    "kitsune fruit" : {
+        "type of fruit": "type of fruit:beast",
+        "moves": "acursed enhansment,tails of burning ahogany"
+    },
+    "bloxfruits" : {
+        "type of game": "fighting"
+    },
 
+}
+game = input("what what fruit do you want to know about?")
+print(bloxfuits[game]["type of fruit"])
 ##################################################
 '''
 Q1: 
 '''
 
 # TODO: Write your code here
-
+sample_dict = {
+   "name": "Hermione",
+   "marks": {
+       "transfiguration": 90,
+       "charms": 98,
+       "potions": 98,
+       "history of magic": 92,
+   }
+}
 ##################################################
 '''
 Q2:
